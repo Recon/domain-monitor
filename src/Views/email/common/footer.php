9264539266147f566e3537db1ca86c3f3febf3f0
Corneliu Popescu <corneliupr@gmail.com>
2016-03-27 11:46:30 +0200

    Added a basic HTML template for emails

---
+++ src/Views/email/common/footer.php
@@ -0,0 +1,36 @@
+</td>
+</tr>
+</table>
+</div>
+
+</td>
+<td></td>
+</tr>
+</table>
+
+<!-- FOOTER -->
+<table class="footer-wrap">
+    <tr>
+        <td></td>
+        <td class="container">
+
+            <!-- content -->
+            <div class="content">
+                <table>
+                    <tr>
+                        <td align="center">
+                            <p>
+                                <a href="#">Website</a>
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </div><!-- /content -->
+
+        </td>
+        <td></td>
+    </tr>
+</table><!-- /FOOTER -->
+
+</body>
+</html>
--- src/Views/email/reset.php
+++ src/Views/email/reset.php
@@ -1,3 +1,10 @@
-To reset your password for the Website Monitor application, use the following link:
+<?php require __DIR__ . '/common/header.php' ?>
 
-<?php echo $recovery_link ?>
+<h3>Password reset</h3>
+<p class="lead">To reset your password for the Website Monitor application, use the following link:</p>
+
+<p class="callout">
+    <a href="<?php echo $recovery_link ?>"><?php echo $recovery_link ?></a>
+</p>
+
+<?php require __DIR__ . '/common/footer.php' ?>
